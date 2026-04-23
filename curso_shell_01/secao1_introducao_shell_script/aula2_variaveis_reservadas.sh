9a5f8d225a95e5f676b247f0c0ea8f1e491a49c6
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-23 17:37:54 -0300

    (curso) variaveis reservadas

---
+++ curso_shell_01/secao1_introducao_shell_script/aula2_variaveis_reservadas.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Variaveis com valores padrções
+
+echo "Parametro 1: $1"
+echo "Parametro 2: $2"
+
+# Ver os parâmetros recebidos
+echo "Todos os parâmetros: $*"
+
+# Ver a quantidade de parâmetros recebidos
+echo "Quantos parâmetros: $#"
+
+# Ultimo comando
+echo "Saída do ultimo comando: $?"
