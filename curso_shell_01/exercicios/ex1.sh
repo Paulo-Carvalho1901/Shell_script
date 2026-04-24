4a17c419fa54c4931bffad6e12dc6a42e3be00d1
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-24 15:32:47 -0300

    (ex) praticando comando variaveis

---
+++ curso_shell_01/exercicios/ex1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Declarando uma variavel
+NOME="Paulo Carvalho"
+IDADE=37
+PESO=92
+
+echo "Meu nome é: $NOME"
+echo "Minha idade é: $IDADE"
+echo "Eu peso $PESO Kg"
+
+# echo $#
+# echo $?
+# echo $$
+echo "Nome do script é: $0"
