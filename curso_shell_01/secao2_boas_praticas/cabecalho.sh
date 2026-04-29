094b48c460b3f72255093c762d86d5914a0e52aa
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-29 14:35:14 -0300

    (doc) padrões de boas praticas no seu projeto shell script

---
+++ curso_shell_01/secao2_boas_praticas/cabecalho.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# nomeDescricao.sh - breve descrição
+
+# site:         http://seusite.com
+# Autor:        Seu nome
+# Manutenção:   Seu nome ou outro
+#------------------------------------------------------------------#
+# Aqui voce deve utilizar uma descrição mais detalhada sobre o seu
+# programa, explicando a forma de utilizar
+
+# Exemplo:
+#     $ ./nomeDoScript.sh -d 1
+#     Neste exemplo o script será executado no modo debug nivel 1
+#------------------------------------------------------------------#
+# Histórico:
+
+#   v1.0 10/08/2018, Autor da mudança:
+#       - O que mudou?
+#   v1.1 19/08/2018, Autor da mudança:
+#       - O que mudou?
+#------------------------------------------------------------------#
+# Testado em:
+#   bash 4.4.19
+#------------------------------------------------------------------#
+# Agradecimentos:
+
+#   Joaozinho - encontrou um bug na parte do login
+#   Mariazinha - enviou sugestão de adicionar o -h
+#-------------------------------------------------------------------#
+
+#-------------------------------------------------------------------#
